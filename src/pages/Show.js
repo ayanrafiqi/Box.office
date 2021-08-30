240c944806f62a7891ee5db4a36d69ad704e52ee
ayan_rafiqi <rafiqiayan@gmail.com>
2021-08-30 15:58:54 +0530

    created loading and error messages

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,19 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { apiGet } from '...misc/config';
-
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../misc/config';
 const Show = () => {
-  const [show, setShow] = useState(null);
-
   const { id } = useParams();
 
+  const [show, setShow] = useState(null);
+  const [isLoading, SetIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    apiGet('/shows/{id}?embed[]=seasons&embed[]=cast').then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          SetIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          SetIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log('show', show);
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+  if (error) {
+    return <div>error occured:{error}</div>;
+  }
 
   return <div>This is a Show page</div>;
 };
