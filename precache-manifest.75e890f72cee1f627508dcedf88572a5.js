f5f58b901c73ceabf0b38f5214c0a3ca4019435a
ayan_rafiqi <rafiqiayan@gmail.com>
2021-09-12 15:28:01 +0530

    Updates

---
+++ precache-manifest.75e890f72cee1f627508dcedf88572a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83f1509447f57080a155ff0977c55ce",
+    "url": "/Box.office/index.html"
+  },
+  {
+    "revision": "24ef14d75cb9aa8750fc",
+    "url": "/Box.office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "2a948197bbb229dead95",
+    "url": "/Box.office/static/js/2.d58ef3e9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Box.office/static/js/2.d58ef3e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6a4f4b9f62ba0c3fd9",
+    "url": "/Box.office/static/js/3.97a87a3b.chunk.js"
+  },
+  {
+    "revision": "24ef14d75cb9aa8750fc",
+    "url": "/Box.office/static/js/main.d68c6329.chunk.js"
+  },
+  {
+    "revision": "5436757c5004c77ca281",
+    "url": "/Box.office/static/js/runtime-main.ad53a301.js"
+  }
+]);
